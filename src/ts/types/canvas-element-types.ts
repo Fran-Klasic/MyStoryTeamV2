345ba231a76ca9dd9dc008e1ab11f1a765b4cbb5
Fran-Klasic <fklasich@gmail.com>
2026-01-21 22:18:59 +0100

    Update

---
+++ src/ts/types/canvas-element-types.ts
@@ -0,0 +1,87 @@
+import type { Vector3Int } from "../vector3int";
+
+export type ID = string;
+
+export type CanvasElement =
+  | {
+      readonly id: ID;
+      readonly type: "Text";
+      data: string;
+      position: Vector3Int;
+      connections: Connection[];
+    }
+  | {
+      readonly id: ID;
+      readonly type: "List";
+      data: ListData;
+      position: Vector3Int;
+      connections: Connection[];
+    }
+  | {
+      readonly id: ID;
+      readonly type: "Image";
+      data: ImageData;
+      position: Vector3Int;
+      connections: Connection[];
+    }
+  | {
+      readonly id: ID;
+      readonly type: "Audio";
+      data: AudioData;
+      position: Vector3Int;
+      connections: Connection[];
+    }
+  | {
+      readonly id: ID;
+      readonly type: "Task";
+      data: TaskData;
+      position: Vector3Int;
+      connections: Connection[];
+    }
+  | {
+      readonly id: ID;
+      readonly type: "Date";
+      data: DateData;
+      position: Vector3Int;
+      connections: Connection[];
+    }
+  | {
+      readonly id: ID;
+      readonly type: "Video";
+      data: VideoData;
+      position: Vector3Int;
+      connections: Connection[];
+    }
+  | {
+      readonly id: ID;
+      readonly type: "Container";
+      data: CanvasElement[];
+      position: Vector3Int;
+      connections: Connection[];
+    };
+
+export type VideoData = {
+  url: string;
+};
+
+export type ListData = {
+  data: string[];
+};
+export type ImageData = {
+  base64File: string;
+};
+export type AudioData = {
+  base64File: string;
+};
+export type TaskData = {
+  data: string;
+  checked: boolean;
+};
+export type DateData = {
+  date: string;
+  data: string;
+};
+export type Connection = {
+  self: ID;
+  target: ID;
+};
---
+++ src/ts/vector3int.ts
@@ -0,0 +1,19 @@
+export class Vector3Int {
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x: number, y: number, z: number) {
+    this.x = Math.round(x);
+    this.y = Math.round(y);
+    this.z = Math.round(z);
+  }
+
+  add(v: Vector3Int): Vector3Int {
+    return new Vector3Int(this.x + v.x, this.y + v.y, 0);
+  }
+
+  equals(v: Vector3Int): boolean {
+    return this.x == v.x && this.y == v.y && this.z == v.z;
+  }
+}
