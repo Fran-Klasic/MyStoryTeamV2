7e106bcb0481fcc7a1da9254efd45f69b31683af
Fran-Klasic <fklasich@gmail.com>
2026-01-22 13:59:47 +0100

    Update

--- src/ts/canvas-base.ts
+++ src/ts/canvas-base.ts
@@ -1,1 +1,15 @@
-const canvas = document.getElementById("canvas");
+//#region DISABLE ZOOM
+window.addEventListener(
+  "wheel",
+  (e) => {
+    if (e.ctrlKey) e.preventDefault();
+  },
+  { passive: false },
+);
+
+window.addEventListener("keydown", (e) => {
+  if (e.ctrlKey && ["+", "-", "0"].includes(e.key)) {
+    e.preventDefault();
+  }
+});
+//#endregion
