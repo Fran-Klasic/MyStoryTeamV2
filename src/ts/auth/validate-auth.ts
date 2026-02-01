f926989b17bb78041c9b2ed9e905f01ec174d949
Fran-Klasic <fklasich@gmail.com>
2026-02-01 15:33:29 +0100

    Update

--- src/ts/auth/validate-auth.ts
+++ src/ts/auth/validate-auth.ts
@@ -2,17 +2,19 @@ import { api } from "../api/api-handler";
 
 const TEST_ENDPOINT = "api/auth/test";
 const USER_DATA_ENDPOINT = "api/auth/getUserData";
+
 const LOGIN_URL = "/html/sign.html";
 
-async function callEndpoint(endpoint: string) {
+async function callEndpoint(endpoint: string): Promise<Response> {
   const response = await api.get(endpoint);
-  const isValid = response.ok;
-  if (isValid === false) {
-    window.location.href = LOGIN_URL;
-  }
+  return response;
 }
 
 window.onload = async () => {
-  callEndpoint(TEST_ENDPOINT);
-  callEndpoint(USER_DATA_ENDPOINT);
+  const response = callEndpoint(TEST_ENDPOINT);
+  const isValid = (await response).ok;
+  if (isValid === false) {
+    window.location.href = LOGIN_URL;
+  }
+  const userData = callEndpoint(USER_DATA_ENDPOINT);
 };
